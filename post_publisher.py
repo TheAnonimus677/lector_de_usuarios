013bd548e8e8007bd3f11b240e85f6b62b443944
gabo-0321 <99088558+gabo-0321@users.noreply.github.com>
2025-12-06 20:06:48 -0400

    Update post_publisher.py

--- post_publisher.py
+++ post_publisher.py
@@ -9,7 +9,7 @@ posting_key = os.getenv("POSTING_KEY")
 
 h = Hive(node=["https://api.hive.blog"], keys=[posting_key])
 
-st.markdown('# Publicador de Posts')
+st.markdown('# Publicador de Posts de PeakD')
 
 with st.form("my_form"):
 
@@ -19,17 +19,17 @@ with st.form("my_form"):
 
     post_content = st.text_area("Ingrese el contenido del post:")
 
-    post_image = st.file_uploader("Ingrese una imagen (solo vista previa)")
+    ## post_image = st.file_uploader("Ingrese una imagen (solo vista previa)")
 
-    post_image_url = st.text_input("URL de la imagen (opcional):")
+    post_image_url = st.text_input("URL de la imagen (obligatorio si deseas postear imágenes):")
 
     post_tags = st.text_input("Ingrese los tags del post (separados por comas):")
 
     sumbit_button = st.form_submit_button(label="Publicar", use_container_width=True)
 
 if sumbit_button:
-    if post_image is not None:
-        st.image(post_image, caption="Vista previa:", use_container_width=False)
+   ## if post_image is not None:
+       ## st.image(post_image, caption="Vista previa:", use_container_width=False)
 
     tags_list = []
     if post_tags:
@@ -41,16 +41,19 @@ if sumbit_button:
             if t and t not in tags_list:
                 tags_list.append(t)
 
-    images_meta = [post_image_url] if post_image_url else []
+    images_meta = [post_image_url] if is not None post_image_url else []
 
     post = h.post(
         title=post_title,
         body=post_content,
         author=post_author,
-        json_metadata=json.dumps({"tags": tags_list, "image": images_meta}),
+        json_metadata={
+            "tags": tags_list, 
+            "image": images_meta
+        },
         blockchain_instance=h
     )
 
-    st.success("Post publicado correctamente")
+    st.success("Post publicado correctamente ✅️")
 
     st.write(f"https://peakd.com/@{post_author}")
